b977a5b9cee62b0d374e6e710401fb5da495d491
Mitch <mitch@hawkins5.com>
2024-02-22 14:13:47 +1100

    Initial git push of blank spring server (with config)

---
+++ mitch_spring_todo/src/main/java/io/nology/mitch_spring_todo/MitchSpringTodoApplication.java
@@ -0,0 +1,13 @@
+package io.nology.mitch_spring_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitchSpringTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitchSpringTodoApplication.class, args);
+	}
+
+}
---
+++ mitch_spring_todo/src/test/java/io/nology/mitch_spring_todo/MitchSpringTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nology.mitch_spring_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitchSpringTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
